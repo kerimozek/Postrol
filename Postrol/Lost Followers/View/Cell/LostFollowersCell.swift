2c3ef92b0f5375d4500e0983d1dacba83cb65795
Kerim <mehmetkerimozek@gmail.com>
2023-01-26 16:07:37 +0300

    added lostFollowers

---
+++ Postrol/Lost Followers/View/Cell/LostFollowersCell.swift
@@ -0,0 +1,23 @@
+//
+//  LostFollowersCell.swift
+//  Postrol
+//
+//  Created by PSI_ErsanV on 26.01.2023.
+//
+
+import UIKit
+
+class LostFollowersCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Postrol/Lost Followers/View/LostFollowersVC.swift
@@ -0,0 +1,23 @@
+//
+//  LostFollowersVC.swift
+//  Postrol
+//
+//  Created by PSI_ErsanV on 26.01.2023.
+//
+
+import UIKit
+
+class LostFollowersVC: UIViewController {
+
+    @IBOutlet weak var lostFollowersTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+  
+
+}
